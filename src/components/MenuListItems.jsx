693ec75a94f2a2e5ba13eb2f8a62bff3ed4c6a1e
Sezer Ökmen <szrokmn34@gmail.com>
2023-04-02 00:44:56 +0300

    title and icon added

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,79 +1,70 @@
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemIcon from "@mui/material/ListItemIcon"
-import ListItemText from "@mui/material/ListItemText"
-import DashboardIcon from "@mui/icons-material/Dashboard"
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
-import InventoryIcon from "@mui/icons-material/Inventory"
-import StoreIcon from "@mui/icons-material/Store"
-import StarsIcon from "@mui/icons-material/Stars"
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
-import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount"
-import { useNavigate } from "react-router-dom"
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import { useNavigate } from "react-router-dom";
 
 const icons = [
-    {
-      icon: <DashboardIcon />,
-      title: "Dashboard",
-      url: "/stock/",
-    },
-    {
-      title: "Purchase",
-      icon: <ShoppingCartIcon />,
-      url: "/stock/purchases/",
-    },
-    {
-      title: "Sales",
-      icon: <AttachMoneyIcon />,
-      url: "/stock/sales/",
-    },
-    {
-      title: "Firms",
-      icon: <StoreIcon />,
-      url: "/stock/firms/",
-    },
-    {
-      title: "Brands",
-      icon: <StarsIcon />,
-      url: "/stock/brands/",
-    },
-    {
-      title: "Products",
-      icon: <InventoryIcon />,
-      url: "/stock/products/",
-    },
-    {
-      title: "Admin Panel",
-      icon: <SupervisorAccountIcon />,
-      url: "https://12126.fullstack.clarusway.com/admin",
-    },
-  ]
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+    url: "/stock/",
+  },
+  {
+    title: "Purchase",
+    icon: <ShoppingCartIcon />,
+    url: "/stock/purchases/",
+  },
+  {
+    title: "Sales",
+    icon: <AttachMoneyIcon />,
+    url: "/stock/sales/",
+  },
+  {
+    title: "Firms",
+    icon: <StoreIcon />,
+    url: "/stock/firms/",
+  },
+  {
+    title: "Brands",
+    icon: <StarsIcon />,
+    url: "/stock/brands/",
+  },
+  {
+    title: "Products",
+    icon: <InventoryIcon />,
+    url: "/stock/products/",
+  },
+  {
+    title: "Admin Panel",
+    icon: <SupervisorAccountIcon />,
+    url: "https://12126.fullstack.clarusway.com/admin",
+  },
+];
 
 const MenuListItems = () => {
   return (
     <div>
-         <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+      <List>
+        {icons?.map((item, index) => (
             <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} />
             </ListItemButton>
-          </ListItem>
         ))}
-      </List>  
+      </List>
     </div>
-  )
-}
+  );
+};
 
-export default MenuListItems
+export default MenuListItems;
