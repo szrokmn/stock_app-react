5abf0593e659b58d9e2f3e354deda104bfad22a5
Sezer Ökmen <szrokmn34@gmail.com>
2023-04-09 00:09:40 +0300

    getProCatBrandSuccess added

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useDispatch, useSelector } from "react-redux"
-import { fetchFail, getSuccess, fetchStart } from "../features/stockSlice"
+import { fetchFail, getSuccess, fetchStart, getProCatBrandSuccess } from "../features/stockSlice"
 import useAxios from "./useAxios"
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
 
@@ -70,7 +70,23 @@ const useStockCall = () => {
     }
   }
 
-  return {getStockData, deleteStockData, postStockData, putStockData}  
+  const getProCatBrand = async () => {
+    dispatch(fetchStart())
+    try {
+    const [products, categories, brands] = await Promise.all([
+      axiosWithToken.get("stock/products/"),
+      axiosWithToken.get("stock/categories/"),
+      axiosWithToken.get("stock/brands/"),
+    ])
+    dispatch(getProCatBrandSuccess())
+    } catch (error) {
+      console.log(error)
+      dispatch(fetchFail())
+      toastErrorNotify(`${url} can not be updated`)
+    }
+  }
+
+  return {getStockData, deleteStockData, postStockData, putStockData, getProCatBrand}  
 }
 
 export default useStockCall
