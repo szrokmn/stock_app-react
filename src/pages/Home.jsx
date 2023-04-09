b74d4e49c9e6c50b0b2383824c63c3b9ffcf5df6
Sezer Ökmen <szrokmn34@gmail.com>
2023-04-09 23:15:15 +0300

    home added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Typography } from "@mui/material"
+import { useEffect } from "react"
+import Charts from "../components/Charts"
+import KpiCards from "../components/KpiCards"
+import useStockCall from "../hooks/useStockCall"
 
 const Home = () => {
+ 
   return (
-    <div>Home</div>
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Dashboard
+      </Typography>
+
+      <KpiCards />
+      <Charts />
+    </div>
   )
 }
 
